8648e5f7b23c37264d27320104f93e4011d4052b
Muhamad Mashudi Ardi Winata <muhamadmashudiardiwinata@gmail.com>
2020-10-23 11:47:21 +0700

    Update order_detail_screen.dart

--- lib/screens/order_detail_screen.dart
+++ lib/screens/order_detail_screen.dart
@@ -1,11 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:futsal_field_jepara_admin/data/data.dart' as data;
 import 'package:futsal_field_jepara_admin/models/user_order.dart';
+import 'package:intl/intl.dart';
 import 'package:lottie/lottie.dart';
+import 'package:supercharged/supercharged.dart';
 
 class OrderDetailScreen extends StatefulWidget {
   final String uid;
@@ -16,6 +21,172 @@ class OrderDetailScreen extends StatefulWidget {
 }
 
 class _OrderDetailScreenState extends State<OrderDetailScreen> {
+  final PrinterBluetoothManager _printerManager = PrinterBluetoothManager();
+  List<PrinterBluetooth> _devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _printerManager.scanResults.listen((devices) async {
+      setState(() {
+        _devices = devices;
+      });
+    });
+  }
+
+  void _startScanDevices() {
+    setState(() {
+      _devices = [];
+    });
+    _printerManager.startScan(4.seconds);
+    _widgetShowDialogLoading(context);
+  }
+
+  void _stopScanDevices() {
+    _printerManager.stopScan();
+    ExtendedNavigator.root.pop();
+  }
+
+  Future<Ticket> ticketFormat(PaperSize paper) async {
+    final ticket = Ticket(paper);
+    final now = DateTime.now();
+    final formatter = DateFormat('dd/MM/yyyy H:m');
+    final timestamp = formatter.format(now);
+
+    ticket.text(
+      'FutsalField\nJepara',
+      styles: PosStyles(
+        align: PosAlign.center,
+        height: PosTextSize.size2,
+        width: PosTextSize.size2,
+      ),
+      linesAfter: 1,
+    );
+
+    ticket.text(
+      timestamp,
+      styles: PosStyles(align: PosAlign.center),
+    );
+
+    ticket.hr();
+
+    await data.loadOrderDetailByOrderUID(widget.uid).then((value) {
+      var _userOrderData = UserOrder.fromMap(value.data());
+
+      ticket.text(
+        'Pemesan : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        _userOrderData.userName,
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.text(
+        'Tanggal Pesan : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        _userOrderData.orderDate,
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.text(
+        'Lapangan Dipesan untuk Jam : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        _userOrderData.orderTime,
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.text(
+        'Nama Lapangan : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        _userOrderData.futsalFieldName,
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.text(
+        'Jenis Lapangan : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        _userOrderData.fieldType,
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.text(
+        'Status Pemesanan : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+        ),
+      );
+      ticket.text(
+        (_userOrderData.orderStatus == 2)
+            ? 'Pesanan Dibatalkan'
+            : (_userOrderData.orderStatus != 0)
+                ? 'Pesanan Diterima'
+                : 'Menunggu',
+        styles: PosStyles(
+          align: PosAlign.right,
+        ),
+      );
+      ticket.hr();
+      ticket.text(
+        'Total Harga : ',
+        styles: PosStyles(
+          align: PosAlign.left,
+          width: PosTextSize.size2,
+          height: PosTextSize.size2,
+        ),
+      );
+      ticket.text(
+        'Rp. ${_userOrderData.fieldType}',
+        styles: PosStyles(
+            align: PosAlign.right,
+            width: PosTextSize.size2,
+            height: PosTextSize.size2),
+      );
+    });
+
+    ticket.feed(2);
+    ticket.cut();
+    return ticket;
+  }
+
+  void _printTicket(PrinterBluetooth printer) async {
+    _printerManager.selectPrinter(printer);
+
+    const paper = PaperSize.mm80;
+
+    final res = await _printerManager
+        .printTicket(await ticketFormat(paper))
+        .then((value) => _widgetShowDialogLoading(context).catchError((error) {
+              print('Log : $error');
+            }).whenComplete(() => ExtendedNavigator.root.pop()));
+
+    print(res.msg);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -155,9 +326,65 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
             ? null
             : (_userOrder.orderStatus == 2)
                 ? null
-                : () {},
+                : () async {
+                    _startScanDevices();
+                    await 4.seconds.delay.then((value) {
+                      _stopScanDevices();
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text(
+                                'Daftar Printer',
+                                textAlign: TextAlign.center,
+                              ),
+                              content: Container(
+                                width: 200,
+                                height: 200,
+                                child: ListView.builder(
+                                  itemCount: _devices.length,
+                                  itemBuilder: (context, index) {
+                                    return Column(
+                                      children: [
+                                        ListTile(
+                                          onTap: () {
+                                            _printTicket(_devices[index]);
+                                            ExtendedNavigator.root.pop();
+                                          },
+                                          title: Text(_devices[index].name),
+                                          leading:
+                                              FaIcon(FontAwesomeIcons.print),
+                                        ),
+                                        Divider(),
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                            );
+                          });
+                    });
+                  },
       ),
     );
+  }
+
+  Future _widgetShowDialogLoading(BuildContext context) {
+    return showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return Container(
+            width: 300,
+            height: 300,
+            child: Center(
+              child: Lottie.asset(
+                'assets/loading.json',
+                height: MediaQuery.of(context).size.height / 100 * 25,
+              ),
+            ),
+          );
+        });
   }
 
   Row _widgetButtonOrderStatus(UserOrder _userOrder, BuildContext context) {
